321f2757b15c1af672290525c96816868d1d69f2
Stefan Hartung <frynsen1986@gmail.com>
2018-12-18 14:57:19 +0100

    Tilføjet model kode til Ordre

--- VoresWebshop/Models/Ordre.cs
+++ VoresWebshop/Models/Ordre.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace VoresWebshop.Models {
-    public class Ordre {
-        }
+namespace VoresWebshop.Models
+{
+    public class Ordre
+    {
+        public int OrdreID { get; set; }
+        public int BrugerID { get; set; }
+        public DateTime Oprettet { get; set; }
+        public DateTime Godkendt { get; set; }
+        public DateTime Afsendt { get; set; }
+        public DateTime Afsluttet { get; set; }
     }
+}
