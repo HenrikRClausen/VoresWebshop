ba47e56e091e385af7013feb66275e592cb78d9b
Henrik Ræder Clausen <henrik@fangorn.dk>
2019-01-02 14:17:15 +0100

    Revert "Revert "Rettede banal fejl i koden. Nu accepterer compileren den.""
    This reverts commit 38388ab2c5a945f3c7f005eda41b47e5da9eb417.

--- VoresWebshop_CORE/Data/ApplicationDbContext.cs
+++ VoresWebshop_CORE/Data/ApplicationDbContext.cs
@@ -21,7 +21,9 @@ namespace VoresWebshop.Data {
 
     public static class VoresWebshopDbInitializer {
         public static void Initialize(IServiceProvider serviceProvider) {
-            using (var context = new VoresWebshopDb)
+            using (var context = new VoresWebshopDb()) {
+
+                }
         }
     }
 
