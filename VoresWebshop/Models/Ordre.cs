3b1258f39dfc9f8a2c8bfab662c51096408372fe
chrisogvic <chrisogvic@gmail.com>
2019-01-02 14:39:44 +0100

    DataAnnotations tilføjet

--- VoresWebshop/Models/Ordre.cs
+++ VoresWebshop/Models/Ordre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace VoresWebshop.Models
     public class Ordre
     {
         public int OrdreID { get; set; }
+
+        [Required]
         public int BrugerID { get; set; }
         public DateTime Oprettet { get; set; }
         public DateTime Godkendt { get; set; }
